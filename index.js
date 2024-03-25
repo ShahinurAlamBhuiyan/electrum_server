fd14a54c46f27356e69c032ef1bcbe5a623f49ca
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-03-25 16:19:41 +0600

    running server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+
+const app = express()
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
