7c7fc818d927dd94ea79f44255bb0697b3a2c650
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-04-29 02:04:23 +0600

    mongodb connection setup

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose'); // Import mongoose
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 
-const app = express()
+const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// shahin branch created
+const MONGODB_URI = process.env.MONGODB_URI;
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB:", err);
+    });
+
+
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
