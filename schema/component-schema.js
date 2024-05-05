f944b8bd242af6f5b2ccd60bf31a60f0c461db44
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-05-05 18:52:54 +0600

    created component-schema

---
+++ schema/component-schema.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const componentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  selling_price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  owner_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User', 
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  buying_price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  type: {
+    type: String,
+    required: true,
+    enum: ['new', 'old'],
+  },
+}, { timestamps: true });
+
+const Components = mongoose.model('Components', componentSchema);
+
+module.exports = Components;
